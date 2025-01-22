5eecbae5c558973e8921f34761c18d9cd684258e
jsam-008 <jlatha541@gmail.com>
2025-01-22 22:17:13 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class natural {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a = sc.nextInt();
+        int sum = (a*(a+1))/2;
+        System.out.println(sum);
+
+    }
+    
+}
