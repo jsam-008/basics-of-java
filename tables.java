0490a8cc892d93b217e2326bcb6ea27c3e377297
jsam-008 <jlatha541@gmail.com>
2025-01-22 21:11:48 +0530

    Add files via upload

---
+++ tables.java
@@ -0,0 +1,8 @@
+class Tables {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
