cf64dd8e5d1080abc0c318cab75f470df58d904d
sam j <jlatha541@gmail.com>
2025-02-01 18:57:50 +0530

    Update and rename greaterno.java to greater_no.java

--- greater_no.java
+++ greater_no.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class greaterno {
+class greater_no {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter the number:");
