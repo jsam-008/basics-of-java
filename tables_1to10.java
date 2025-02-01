3771963ebb9317f0959f655c7e03d7d281a581b1
sam j <jlatha541@gmail.com>
2025-02-01 18:58:54 +0530

    Update and rename tables10.java to tables_1to10.java

--- tables_1to10.java
+++ tables_1to10.java
@@ -1,4 +1,4 @@
-class tables10 {
+class tables_1to10 {
         public static void main(String[] args) {
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
